29307b1e2f811314fadcc4a12201c673aad0ba9e
GGhislandi <giulio.ghislandi@itsrizzoli.it>
2024-07-11 10:18:31 +0200

    aggiunta bozza di query

--- db.py
+++ db.py
@@ -3,18 +3,30 @@ import neo4j
 class Cells_db:
 
     def __init__(self) -> None:
-    
-        # Replace with your Neo4j credentials and connection details
-        uri = "bolt://localhost:7687"  # Default URI for local Neo4j instance
+        
+        # credenziali
+        uri = "bolt://localhost:7687"
         username = "neo4j"
         password = "id64eRyHBQL5Cvw"
 
-        # Create a driver instance
-        driver = neo4j.GraphDatabase.driver(uri, auth=(username, password))
+        # creazione del driver
+        self.driver = neo4j.GraphDatabase.driver(uri, auth=(username, password))
+        self.driver.verify_connectivity()
 
-        if not driver.verify_connectivity():
-            print("Connessione al db riuscita!")
+        # nome del db sul server (default)
+        self.database = "neo4j"
 
+
+    def get_nodes(self):
+
+        # dalla guida ufficiale online: 
+        # " Do not hardcode or concatenate parameters: use placeholders and specify the parameters as keyword arguments. "
+        
+        records, summary, keys = self.driver.execute_query(
+                                                            "MATCH (p:Person {age: $age}) RETURN p.name AS name",
+                                                            age=42,
+                                                            database_=self.database,
+                                                        )
 
 if __name__ == "__main__":
 
